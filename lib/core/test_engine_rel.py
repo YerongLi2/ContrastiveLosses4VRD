4a1b0919020d595560f2b437f12d7628b994cda8
Yerong Li <yerongli.ads@outlook.com>
2022-04-04 01:56:39 -0500

    Move sys after the __ imports

--- lib/core/test_engine_rel.py
+++ lib/core/test_engine_rel.py
@@ -18,12 +18,13 @@
 ##############################################################################
 
 """Test a Detectron network on an imdb (image database)."""
-import sys
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import sys
 from collections import defaultdict
 import cv2
 import datetime
