485323da30437c4b450277e129db89d8650e589f
Yerong Li <yerongli.ads@outlook.com>
2022-04-04 02:55:00 -0500

    Print length of the value

--- load.py
+++ load.py
@@ -1,4 +1,6 @@
 import json
 filename = './data/vrd/train_fname_mapping.json'
 data = json.load(open(filename, 'r'))
-print(data)
+print(len(data.keys()))
+print(len(set(data.values())))
+# print(data)
