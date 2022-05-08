de281c54230141b7733f810e146d3aef96cfe52a
Yerong Li <yerong.li@outlook.com>
2022-05-08 03:30:55 -0500

    load 2315355_0.png

--- load.py
+++ load.py
@@ -1,6 +1,6 @@
 import json
 import os
-filename = os.getenv('HOME')+'/data/vrd/rel_annotations_val.json'
+filename = os.getenv('HOME')+'/data/vg/rel_annotations_val.json'
 data = json.load(open(filename, 'r'))
 print(data['2315355.jpg'])
 # print(len(set(data.values())))
