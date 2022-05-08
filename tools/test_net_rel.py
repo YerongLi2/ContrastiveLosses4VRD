f8efdacf19d62871eb29600453d1733f75cc45d2
Yerong Li <yerong.li@outlook.com>
2022-05-08 01:06:34 -0500

    Use all to check equal arrays

--- tools/test_net_rel.py
+++ tools/test_net_rel.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
             det_file = os.path.join(args.output_dir, 'rel_detections_gt_boxes_sgcls.pkl')
     else:
         det_file = os.path.join(args.output_dir, 'rel_detections.pkl')
-    if  os.path.exists(det_file): ## DEBUG
+    if False and  os.path.exists(det_file): ## DEBUG
         logger.info('Loading results from {}'.format(det_file))
         with open(det_file, 'rb') as f:
             all_results = pickle.load(f)
